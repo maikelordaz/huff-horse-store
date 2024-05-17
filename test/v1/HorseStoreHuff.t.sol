d6ab4b81f08e984fb9b5ba2700d3c8d5947984d4
Maikel Ordaz <maikelordaz@gmail.com>
2024-05-17 10:04:11 -0400

    tests

---
+++ test/v1/HorseStoreHuff.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {Base_TestV1, HorseStore} from "./Base_TestV1.t.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+contract HorseStoreHuff is Base_TestV1 {
+    // It assumes it is always on src folder. So the path is without src and without .huff extension
+    string public constant horseStoreLocation = "horseStoreV1/HorseStore";
+
+    function setUp() public override {
+        // horseStore = HorseStore(
+        //     HuffDeployer.config().deploy(HORSE_STORE_HUFF_LOCATION)
+        // );
+        horseStore = HorseStore(
+            HuffDeployer.config().deploy(horseStoreLocation)
+        );
+    }
+}
